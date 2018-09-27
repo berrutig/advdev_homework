300409b065eed22999cffa59566854f7fd8e0850
dule22 <dusan.culibrk@live.com>
2018-09-27 11:23:22 +0200

    init commit

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -12,18 +12,38 @@ REPO=$2
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
-# Code to set up the Jenkins project to execute the
-# three pipelines.
-# This will need to also build the custom Maven Slave Pod
-# Image to be used in the pipelines.
-# Finally the script needs to create three OpenShift Build
-# Configurations in the Jenkins Project to build the
-# three micro services. Expected name of the build configs:
-# * mlbparks-pipeline
-# * nationalparks-pipeline
-# * parksmap-pipeline
-# The build configurations need to have two environment variables to be passed to the Pipeline:
-# * GUID: the GUID used in all the projects
-# * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
+# Create master Jenkins build
+#oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=5Gi --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
 
-# To be Implemented by Student
+# Adjust readiness probe for Jenkins
+oc set probe dc jenkins --readiness --initial-delay-seconds=300 -n ${GUID}-jenkins
+
+# Setup Jenkins Mavin ImageStream for Jenkins slave builds
+oc new-build --name=jenkins-slave-maven-skopeo-centos7 -D $'FROM openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && yum clean all\nUSER 1001' -n ${GUID}-jenkins
+
+# Sleep 30 seconds for Image Stream to be created
+sleep 30
+
+# Tag Jenkins slave ImageStream to latest
+oc tag jenkins-slave-maven-skopeo-centos7 jenkins-slave-maven-skopeo-centos7:latest -n ${GUID}-jenkins
+
+# Wait for Jenkins to fully deploy and become ready
+while : ; do
+  echo "Checking if Jenkins pod is Ready..."
+  oc get pod -n ${GUID}-jenkins | grep -v "deploy\|build" | grep -q "1/1"
+  [[ "$?" == "1" ]] || break
+  echo "... not quite yet. Sleeping 20 seconds."
+  sleep 20
+done
+
+# Add version 3.9 tag to Jenkins slave ImageStream
+oc tag jenkins-slave-maven-skopeo-centos7:latest jenkins-slave-maven-skopeo-centos7:v3.9 -n ${GUID}-jenkins
+
+# Label Jenkins slave ImageStream for Jenkins to use for slave builds
+oc label imagestream jenkins-slave-maven-skopeo-centos7 role=jenkins-slave -n ${GUID}-jenkins
+
+# Create the three pipeline build configs
+sed "s/GUID_VARIABLE/${GUID}/g;s/CLUSTER_VARIABLE/${CLUSTER}/g" ./Infrastructure/templates/mlbparks-pipeline_template_build.yaml | oc process -f - | oc create -f - -n ${GUID}-jenkins
+sed "s/GUID_VARIABLE/${GUID}/g;s/CLUSTER_VARIABLE/${CLUSTER}/g" ./Infrastructure/templates/nationalparks-pipeline_template_build.yaml | oc process -f - | oc create -f - -n ${GUID}-jenkins
+sed "s/GUID_VARIABLE/${GUID}/g;s/CLUSTER_VARIABLE/${CLUSTER}/g" ./Infrastructure/templates/parksmap-pipeline_template_build.yaml | oc process -f - | oc create -f - -n ${GUID}-jenkins
