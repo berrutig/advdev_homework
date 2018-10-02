1040d298f45c6949e43142c7619e01f6e39bc8a3
dulecux <dusan.culibrk@live.com>
2018-10-02 11:13:06 +0200

    Update setup.sh

--- Infrastructure/bin/setup.sh
+++ Infrastructure/bin/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./setup_projects.sh sjl dusan.culibrk-devoteam.com
-./setup_sonar.sh sjl
-./setup_jenkins.sh sjl https://github.com/dulecux/advdev_homework na39.openshift.opentlc.com
-./setup_dev.sh sjl
-./setup_prod.sh sjl
-./setup_nexus.sh sjl
+./setup_projects.sh 89a4 dusan.culibrk-devoteam.com
+./setup_sonar.sh 89a4
+./setup_jenkins.sh 89a4 https://github.com/dulecux/advdev_homework na39.openshift.opentlc.com
+./setup_dev.sh 89a4
+./setup_prod.sh 89a4
+./setup_nexus.sh 89a4
 
