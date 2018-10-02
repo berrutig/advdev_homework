757ed254c6db8b00ae912f55adc096c028e2f462
dulecux <dusan.culibrk@live.com>
2018-10-02 11:10:49 +0200

    Update setup.sh

--- Infrastructure/bin/setup.sh
+++ Infrastructure/bin/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./setup_projects.sh sjl slibonat-redhat.com
+./setup_projects.sh sjl dusan.culibrk-devoteam.com
 ./setup_sonar.sh sjl
-./setup_jenkins.sh sjl https://github.com/slibonati/advdev_homework_template na39.openshift.opentlc.com
+./setup_jenkins.sh sjl https://github.com/dulecux/advdev_homework na39.openshift.opentlc.com
 ./setup_dev.sh sjl
 ./setup_prod.sh sjl
 ./setup_nexus.sh sjl
